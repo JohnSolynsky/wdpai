025f0377f278784d5dc4a4d2b056909ac50ffb53
JohnSolynsky <johnsolynsky@gmail.com>
2024-10-25 10:41:50 +0200

    first_commit

--- python_server/server.py
+++ python_server/server.py
@@ -7,6 +7,12 @@ from typing import Type
 # This class will handle HTTP requests that the server receives.
 class SimpleRequestHandler(BaseHTTPRequestHandler):
 
+    user_list = [{
+        'first_name': 'Jan',
+        'last_name': 'Soliński',
+        'role': 'instructor'
+    }]
+
     # Handle OPTIONS requests (used in CORS preflight checks).
     # CORS (Cross-Origin Resource Sharing) is a mechanism that allows restricted resources
     # on a web page to be requested from another domain outside the domain from which the resource originated.
@@ -43,11 +49,8 @@ class SimpleRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Prepare the response data, which will be returned in JSON format.
-        # The response contains a simple message and the path of the request.
-        response: dict = {
-            "message": "This is a GET request",
-            "path": self.path
-        }
+        response: list
+        response = self.user_list
 
         # Convert the response dictionary to a JSON string and send it back to the client.
         # `self.wfile.write()` is used to send the response body.
@@ -68,10 +71,10 @@ class SimpleRequestHandler(BaseHTTPRequestHandler):
         received_data: dict = json.loads(post_data.decode())
 
         # Prepare the response data.
-        # It includes a message indicating it's a POST request and the data we received from the client.
         response: dict = {
-            "message": "This is a POST request",
-            "received": received_data
+            "first_name": received_data['first_name'],
+            "last_name": received_data['last_name'],
+            "role": received_data['role']
         }
 
         # Send the response headers.
@@ -88,6 +91,46 @@ class SimpleRequestHandler(BaseHTTPRequestHandler):
         # Convert the response dictionary to a JSON string and send it back to the client.
         self.wfile.write(json.dumps(response).encode())
 
+        # Append the response data to variable
+        self.user_list.append(response)
+
+    # Handle DELETE requests.
+    # This method is called when the client sends a DELETE request.
+    def do_DELETE(self) -> None:
+        # Retrieve the content length from the request headers.
+        # This tells us how much data to read from the request body.
+        content_length: int = int(self.headers['Content-Length'])
+
+        # Read the request body based on the content length.
+        post_data: bytes = self.rfile.read(content_length)
+
+        # Decode the received byte data and parse it as JSON.
+        # We expect the DELETE request body to contain JSON-formatted data.
+        received_data: dict = json.loads(post_data.decode())
+
+        # Prepare the response data.
+        response: dict = {
+            "first_name": received_data['first_name'],
+            "last_name": received_data['last_name'],
+            "role": received_data['role']
+        }
+
+        # Send the response headers.
+        # Set the status to 200 OK and indicate the response content will be in JSON format.
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+
+        # Again, allow any origin to access this resource (CORS header).
+        self.send_header('Access-Control-Allow-Origin', '*')
+
+        # Finish sending headers.
+        self.end_headers()
+
+        # Convert the response dictionary to a JSON string and send it back to the client.
+        self.wfile.write(json.dumps(response).encode())
+
+        # Remove the response data from variable
+        self.user_list.remove(response)
 
 # Function to start the server.
 # It takes parameters to specify the server class, handler class, and port number.
